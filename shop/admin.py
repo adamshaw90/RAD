e8c9e02a8a89a866ec1fc8a20b0541de34c9ec19
adamshaw90 <adamoshaw90@gmail.com>
2025-02-24 16:59:41 +1100

    Add admin classes to the shop/admin.py file - product, order and orderitem

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Product, Order, OrderItem
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'created_at')
+    search_fields = ('name',)
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total', 'paid', 'created_at')
+    list_filter = ('paid', 'created_at')
+    search_fields = ('user__username',)
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity')
