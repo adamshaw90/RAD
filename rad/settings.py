39f64e256a7e50f4abc444eb9f3b818343003415
adamshaw90 <adamoshaw90@gmail.com>
2025-02-24 15:13:04 +1100

    Update the typo in Procfile

--- rad/settings.py
+++ rad/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rad-47d779d14a28.herokuapp.com']
+ALLOWED_HOSTS = ['rad-47d779d14a28.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
