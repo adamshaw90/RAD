c5105d6303f8c74b9e69ffa282a050178a9132aa
adamshaw90 <adamoshaw90@gmail.com>
2025-02-25 13:51:03 +1100

    Add postgres database to env.py

--- env.py
+++ env.py
@@ -2,6 +2,7 @@ import os
 
 os.environ.setdefault('SECRET_KEY', 'eqG7DhJIhQfNsz0SJnbDg6sRXApgIDzX')
 
+os.environ['DATABASE_URL'] = 'postgresql://neondb_owner:Dgitr6R0oyhV@ep-dark-bar-a2st7aof.eu-central-1.aws.neon.tech/shred_king_quail_26107'
 
 os.environ['CLOUDINARY_CLOUD_NAME'] = 'dxwiahivd'
 os.environ['CLOUDINARY_API_KEY'] = '371484113584354'
