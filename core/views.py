353caf0abe03211976e7f48b30f1300f12902e5e
adamshaw90 <adamoshaw90@gmail.com>
2025-02-25 17:01:58 +1100

    Update the core views.py file to import ContactMessage from models

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from shop.models import Product
 from .forms import ContactForm
+from .models import ContactMessage
 
 
 def home(request):
@@ -16,19 +17,16 @@ def contact(request):
     if request.method == "POST":
         form = ContactForm(request.POST)
         if form.is_valid():
-            # Process the form data here:
-            # For example, send an email, save the enquiry in the database, etc.
-            # name = form.cleaned_data['name']
-            # email = form.cleaned_data['email']
-            # enquiry = form.cleaned_data['enquiry']
-            # (Do something with the data)
-
-            # Optionally, redirect to a thank-you page or render a success message
+            # Save the message to the database
+            ContactMessage.objects.create(
+                name=form.cleaned_data['name'],
+                email=form.cleaned_data['email'],
+                enquiry=form.cleaned_data['enquiry']
+            )
             return redirect('home')
     else:
         form = ContactForm()
     return render(request, 'core/contact.html', {'form': form})
-
 
 
 def wholesale(request):
