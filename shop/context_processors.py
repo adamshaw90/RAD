822b82be02f86a6ead71888eb42a0b87f9c30634
adamshaw90 <adamoshaw90@gmail.com>
2025-03-04 14:01:33 +1100

    Update the context processor file to return the cart total to a decimal place of 2, this fixes the issue of too many decimal places being diplayed for a breif second whilst the cart is updating

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -1,10 +1,9 @@
-from .models import Product
 from decimal import Decimal
 
 
 def cart_total(request):
     cart = request.session.get('cart', {})
-    
+
     total = sum(Decimal(item['price']) * item['quantity'] for item in cart.values())
 
-    return {'cart_total': total}
+    return {'cart_total': round(total, 2)}
