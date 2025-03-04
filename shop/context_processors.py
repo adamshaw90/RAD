f8977cb948d84ee389cbcdba4cb0dd773bbb5b01
adamshaw90 <adamoshaw90@gmail.com>
2025-03-04 14:19:05 +1100

    Update context processor file to ensure item is a dictionary, this will fix an Heroku error that wont run the app as it was not checking this

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -4,6 +4,9 @@ from decimal import Decimal
 def cart_total(request):
     cart = request.session.get('cart', {})
 
-    total = sum(Decimal(item['price']) * item['quantity'] for item in cart.values())
+    total = sum(
+        Decimal(item['price']) * item['quantity']
+        for item in cart.values() if isinstance(item, dict)  # ✅ Ensure item is a dictionary
+    )
 
     return {'cart_total': round(total, 2)}
