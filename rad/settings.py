f45945ca369977abd7c9cc0378795b7f051b2172
adamshaw90 <adamoshaw90@gmail.com>
2025-03-26 21:29:14 +0800

    Set debug to false

--- rad/settings.py
+++ rad/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rad-47d779d14a28.herokuapp.com', '127.0.0.1', 'localhost']
 
